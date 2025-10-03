9ccb895c53a552cad06068e4c0cf57ca2080beb1
jairojdelahoz <123215829+jairojdelahoz@users.noreply.github.com>
2025-10-03 08:48:10 -0500

    ejercicios en clase

---
+++ ejercicios_en_clase/ejercicio2.py
@@ -0,0 +1,4 @@
+print("ingresa el radio del circulo")
+r = float(input())
+area = 3.1416 * pow (r,2)
+print("el area del circulo segun su radio es: ", area)
---
+++ ejercicios_en_clase/ejercicio3.py
@@ -0,0 +1,6 @@
+n = input("escribe el nombre del producto")
+print("escribe el precio del producto")
+c = input()
+resultado = c * 1.20
+resultado2 = resultado*0.15
+print("el precio final", c, "mas impuesto y el 120% es: ",resultado2)
