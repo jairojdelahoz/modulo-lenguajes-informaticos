a8a0a9b02b964f15b64b3da6c68742c53af91879
jairojdelahoz <123215829+jairojdelahoz@users.noreply.github.com>
2025-10-31 09:10:00 -0500

    Delete ejercicios_en_clase directory

--- ejercicios_en_clase/ejercicio1.py
+++
@@ -1,6 +0,0 @@
-import math
-print("Ingrese un numero real")
-number = float(input())
-raizC = math.sqrt(number) 
-raiz4 = math.sqrt(raizC)
-print("la raiz caudrada del numero: ", raizC ," y su raiz cubica es", raiz4)
--- ejercicios_en_clase/ejercicio3.py
+++
@@ -1,6 +0,0 @@
-n = input("escribe el nombre del producto")
-print("escribe el precio del producto")
-c = input()
-resultado = c * 1.20
-resultado2 = resultado*0.15
-print("el precio final", c, "mas impuesto y el 120% es: ",resultado2)
